c75a0a83d30ba560733982dc055b83efd932af2a
Mike Panitz <mike.panitz@gmail.com>
2019-06-17 21:55:19 -0700

    Initial commit

---
+++ GettingStartedFollowAlong/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GettingStartedFollowAlong
+//
+//  Created by Michael Panitz on 6/17/19.
+//  Copyright © 2019 Michael Panitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
